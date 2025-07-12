d90521e909995a05eea94fb840c0d932b61eb735
keqingli1129 <keqingli1129@gmail.com>
2025-07-12 14:21:22 -0500

    another test

--- rl.py
+++ rl.py
@@ -16,4 +16,7 @@ print("observation space shape:", env.observation_space.shape)
 print("sample observation:", env.observation_space.sample())
 print("sample observation:", env.observation_space.sample())
 print("sample observation:", env.observation_space.sample())
+print("sample observation:", env.observation_space.sample())
+print("sample observation:", env.observation_space.sample())
+
 env.close()
