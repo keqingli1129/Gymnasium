dfc2019ea85b6cf2f1bab6dec9d56a833fcd7537
keqingli1129 <keqingli1129@gmail.com>
2025-07-19 18:34:56 -0500

    TEST

--- rl.py
+++ rl.py
@@ -7,17 +7,7 @@ env.reset()
 
 # sample action:
 print("sample action shape:", env.action_space.shape)
-print("sample action:", env.action_space.sample())
 
 # observation space shape:
 print("observation space shape:", env.observation_space.shape)
-
-# sample observation:
-print("sample observation:", env.observation_space.sample())
-print("sample observation:", env.observation_space.sample())
-print("sample observation:", env.observation_space.sample())
-print("sample observation:", env.observation_space.sample())
-print("sample observation:", env.observation_space.sample())
-print("sample observation:", env.observation_space.sample())
-
 env.close()
